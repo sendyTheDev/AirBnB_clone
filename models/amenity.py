a790a305506d8bfbb4f1a023ea57fefd99c9ed66
sendyTheDev <phathisanisendy@gmail.com>
2023-08-12 12:30:18 -0700

    AirBnB_clone

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Defines the amenity class"""
-from .base_model import BaseModel
 
+from models.base_model import BaseModel
 
 class Amenity(BaseModel):
     """A blueprint for amenity objects"""
+
     name = ""
---
+++ tests/test_console.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Defines unittests for console.py"""
+import unittest
+from models import storage
+from models.engine.file_storage import FileStorage
+from console import HBNBCommand
+from unittest.mock import patch
+
+
+class TestHBNBCommand_help(unittest.TestCase):
+    """Unittests for testing help messages of the HBNB"""
+
+    def test_help_quit(self):
+        Q = "Quit command to exit the program."
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("help quit"))
+            self.assertEqual(h, output.getvalue().strip())
+
+    def test_help_create(self):
+        Q = ("Usage: create <class>\n        "
+            "Create a new class instance and print its id.")
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("help create"))
+            self.assertEqual(h, output.getvalue().strip())
+
+    def test_help(self):
+        Q = ("Documented commands (type help <topic>):\n"
+            "========================================\n"
+            "EOF  all  count  create  destroy  help  quit  show  update")
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("help"))
+            self.assertEqual(h, output.getvalue().strip())
+
+class TestHBNBCommand_create(unittest.TestCase):
+    """Unittests for testing create from the HBNB command interpreter."""
+
+    @classmethod
+    def setUp(self):
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
+        FileStorage.__objects = {}
